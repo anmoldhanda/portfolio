f83a09768703f68a67ea2bb6da16bd95f3011010
anmol dhanda <rk352714@gmail.com>
2024-03-25 20:06:51 +0530

    logo updated swiperjs disabled code description comments good practice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
-// ================== navigation menu ==================
+// ==================================== main logo text ====================================
+const logo_div = document.querySelector(".logo");
+const anchor_tag = document.createElement("a");
+let textnode = document.createTextNode("</> anmol dhanda");
+anchor_tag.setAttribute("href", "#");
+anchor_tag.className = "logotext";
+anchor_tag.appendChild(textnode);
+logo_div.appendChild(anchor_tag);
+
+// ==================================== navigation menu ====================================
 const hamburger = document.querySelector(".hamburger");
 const navmenuiconul = document.querySelector(".navmenuiconul");
 const navmenuicon = document.querySelectorAll(".navmenuicon");
@@ -14,7 +23,7 @@ navmenuicon.forEach((link) => {
   });
 });
 
-// ================== work experience jobs modal ==================
+// ==================================== work experience jobs modal ====================================
 let alphapopupcard = document.querySelector(".alphapopupcard");
 let zealpopupcard = document.querySelector(".zealpopupcard");
 let alphaopenpopupbtn = document.getElementById("alphaopenpopupbtn");
@@ -45,40 +54,40 @@ zealclosepopupbtn.addEventListener("click", () => {
   overlay.style.display = "none";
 });
 
-// ================== workspace projects slider  ==================
-let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  grabCursor: true,
-  loop: true,
-  breakpoints: {
-    50: {
-      slidesPerView: 1,
-    },
-    320: {
-      slidesPerView: 1,
-    },
-    481: {
-      slidesPerView: 1,
-    },
-    769: {
-      slidesPerView: 2,
-    },
-    992: {
-      slidesPerView: 3,
-    },
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// ==================================== workspace projects slider  ====================================
+// let swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 2,
+//   spaceBetween: 30,
+//   grabCursor: true,
+//   loop: true,
+//   breakpoints: {
+//     50: {
+//       slidesPerView: 1,
+//     },
+//     320: {
+//       slidesPerView: 1,
+//     },
+//     481: {
+//       slidesPerView: 1,
+//     },
+//     769: {
+//       slidesPerView: 2,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//     },
+//   },
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
-// ================== dark light theme  ==================
+// ==================================== dark light theme  ====================================
 let lightmode = document.getElementById("lightmode");
 let darkmode = document.getElementById("darkmode");
 darkmode.addEventListener("click", function darktheme() {
@@ -93,7 +102,7 @@ lightmode.addEventListener("click", function lighttheme() {
   lightmode.classList.add("current-theme");
 });
 
-// ================== contact form modal  ==================
+// ==================================== contact form modal  ====================================
 let contactformpopupcontainer = document.querySelector(
   ".contactformpopupcontainer"
 );
@@ -112,7 +121,7 @@ closecontactpopup.addEventListener("click", () => {
   document.body.classList.remove("active");
 });
 
-// ================== contact form modal validation  ==================
+// ==================================== contact form modal validation  ====================================
 const inputname = document.getElementById("inputname");
 const inputemail = document.getElementById("inputemail");
 const inputsubject = document.getElementById("inputsubject");
